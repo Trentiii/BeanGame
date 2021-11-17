29dc6bf424cad6c71b47290ac15aa43c6994f855
MUKILTEO\1707241 <1707241@mukilteo.wednet.edu>
2021-11-17 10:22:34 -0800

    Made a working enemy in my scene. Needs some adjustments still but its functional.

---
+++ Assets/Trent/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+    public Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBtwShots = startTimeBtwShots;
+    }
+
+
+    
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        } else if(Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        } else if(Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+
+        if(timeBtwShots <= 0)
+        {
+            GameObject Clone = Instantiate(projectile, transform.position, Quaternion.identity);
+            Clone.GetComponent<Rigidbody2D>().AddForce((player.transform.position - transform.position).normalized * 1000);
+            Destroy(Clone, 5);
+            timeBtwShots = startTimeBtwShots;
+        } else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
