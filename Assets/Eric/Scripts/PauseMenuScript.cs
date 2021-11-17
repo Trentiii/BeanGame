56fb24f144e7a1f9d52f0281df20647e1ea5e2aa
MUKILTEO\1707234 <1707234@mukilteo.wednet.edu>
2021-11-17 10:15:52 -0800

    Changes to Pause menu (working non-blur version)

---
+++ Assets/Eric/Scripts/PauseMenuScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public GameObject PausePanel;
+    public Animator ani;
+    public bool pauseOpen = false;
+    public float openCoolDown = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (pauseOpen && Time.time > openCoolDown)
+            {
+                StartCoroutine(closePauseMenu());
+                openCoolDown = Time.time + 0.42f;
+                pauseOpen = !pauseOpen;
+            }
+            else if(!pauseOpen && Time.time > openCoolDown)
+            {
+                PausePanel.SetActive(true);
+                openCoolDown = Time.time + 0.42f;
+                pauseOpen = !pauseOpen;
+            }
+        }
+        
+    }
+
+    IEnumerator closePauseMenu()
+    {
+        ani.SetTrigger("PauseDone");
+
+        yield return new WaitForSeconds(0.5f);
+
+        PausePanel.SetActive(false);
+    }
+}
