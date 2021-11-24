79cfbba939844ac16f40c5dbf2b4bb4edac7b287
EricRoman16 <1707234@mukilteo.wednet.edu>
2021-11-23 17:29:49 -0800

    Bunch of stuff with Main Menu - Eric

--- Assets/Eric/Scripts/MainMenuScript.cs
+++ Assets/Eric/Scripts/MainMenuScript.cs
@@ -5,18 +5,26 @@ using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
-    Color Discusting = new Color(0.69f,0.1f,0.1f,1);
-    Color Normal = new Color(1, 1, 1, 1);
+    public Animator ani;
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<Image>().color = Normal;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.GetComponent<Image>().color = Color.Lerp(Normal, Discusting, 2 * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ani.SetTrigger("Beat");
+            Debug.Log("Beating...   Keycode : Space");
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ani.SetTrigger("ToDisgust");
+            Debug.Log("Fading to disgust...    Keycode : P");
+        }
     }
     
 }
