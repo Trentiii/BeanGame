71658a2252d75d160ebaff45ade0d2ec6643c5c2
MUKILTEO\1707234 <1707234@mukilteo.wednet.edu>
2022-01-10 09:48:07 -0800

    cleaning up my folder and working on pause menu finally

---
+++ Assets/Eric/Scripts/PauseMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public GameObject PausePanel;
+    private Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = PausePanel.GetComponent<Animator>();
+        PausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
