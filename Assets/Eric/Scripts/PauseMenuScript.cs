17d213c597ff83b137a5bbe244a0de45b7b78715
MUKILTEO\1707234 <1707234@mukilteo.wednet.edu>
2022-01-10 10:20:32 -0800

    Almost got pause menu working

--- Assets/Eric/Scripts/PauseMenuScript.cs
+++ Assets/Eric/Scripts/PauseMenuScript.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class PauseMenuScript : MonoBehaviour
 {
+    public GameObject SceneTransitioner;
+    public Canvas MainCanvas;
     public GameObject PausePanel;
     private Animator ani;
+    public GameObject GV;
+    public bool Paused = false;
     // Start is called before the first frame update
     void Start()
     {
+        MainCanvas.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
         ani = PausePanel.GetComponent<Animator>();
         PausePanel.SetActive(false);
     }
@@ -16,6 +21,46 @@ public class PauseMenuScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        if (Paused)
+        {
+            //play fading away animation
+            PausePanel.SetActive(false);
+            GV.SetActive(false);
+        }
+        else
+        {
+            //play sliding in animation
+            PausePanel.SetActive(true);
+            GV.SetActive(true);
+        }
+
+        Paused = !Paused;
+    }
+
+    public void creditsClicked()// if menu or credits clicked
+    {
+        MainCanvas.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceCamera;
+        //call transition to credit scene
+        SceneTransitioner.GetComponent<SceneTransitionScript>().creditScene();
+    }
+
+    public void MenuClicked()// if menu or credits clicked
+    {
+        MainCanvas.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceCamera;
+        //call transition to main menu
+        SceneTransitioner.GetComponent<SceneTransitionScript>().mainMenuScene();
+    }
+
+    public void xButtonClicked() // might not be working
+    {
+        PauseGame();
     }
 }
