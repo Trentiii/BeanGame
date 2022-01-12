f19d35f446f0b3cdcf1ed409d0316ae11939849b
MUKILTEO\1707241 <1707241@mukilteo.wednet.edu>
2022-01-12 10:18:38 -0800

    no message

--- Assets/Trent/Sprites/Enemies/Civilian/Civilian.cs
+++ Assets/Trent/Sprites/Enemies/Civilian/Civilian.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Civilian : MonoBehaviour
 {
+
+
+
+    public enum State
+    {
+        idle,
+        patrol,
+        run,
+    }
+    public State currentState = State.patrol;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +25,26 @@ public class Civilian : MonoBehaviour
     {
         
     }
+
+    private void Run()
+    {
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+       /*/Radius for sight
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSight);
+        //Radius for attacking
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackDistance);
+        //Radius for retreating
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, retreatDistance);
+        //Raycast ground detection
+        Gizmos.color = Color.yellow;
+        if (player != null) Gizmos.DrawLine(transform.position, player.position);
+
+    }*/
 }
