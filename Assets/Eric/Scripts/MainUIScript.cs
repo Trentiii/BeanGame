b59a99e1efd83b041fca8f2b5f548af5dc094e45
EricRoman16 <1707234@mukilteo.wednet.edu>
2022-01-16 23:01:41 -0800

    enemy counter should also now be working sorta

--- Assets/Eric/Scripts/MainUIScript.cs
+++ Assets/Eric/Scripts/MainUIScript.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainUIScript : MonoBehaviour
 {
     public GameObject slider;
+    public TMP_Text UICounter;
+    private int TotalEnemies = 0;
+    private int EnemiesLeft = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +20,17 @@ public class MainUIScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UICounter.text = "Enemies left: " + EnemiesLeft + " / " + TotalEnemies;
         slider.GetComponent<Slider>().value = PlayerHealth.playerHealth;
+    }
+
+    public void setTotalEnemies(int enemies)
+    {
+        TotalEnemies = enemies;
+    }
+
+    public void setEnemiesLeft(int enemies)
+    {
+        EnemiesLeft = enemies;
     }
 }
