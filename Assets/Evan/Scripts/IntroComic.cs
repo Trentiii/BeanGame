928b22239132415a8f1a0d75186321aabf23781f
ChumaDaCabbage <1707224@mukilteo.wednet.edu>
2022-01-16 16:17:17 -0800

    Started intro comic

---
+++ Assets/Evan/Scripts/IntroComic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroComic : MonoBehaviour
+{
+    public Faucet Faucet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startComic()
+    { 
+    
+    }
+}
