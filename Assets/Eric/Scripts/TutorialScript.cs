15120240bd5a6571e71fc6349d23d5ff0c5a0d11
ChumaDaCabbage <1707224@mukilteo.wednet.edu>
2022-01-19 11:50:18 -0800

    Tutroials done and civ particles done

--- Assets/Eric/Scripts/TutorialScript.cs
+++ Assets/Eric/Scripts/TutorialScript.cs
@@ -1,22 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TutorialScript : MonoBehaviour
 {
     public GameObject mainTutorial;
+    private TMP_Text tmpt;
 
-
+    bool on = false;
 
     // Start is called before the first frame update
     void Start()
     {
         mainTutorial.SetActive(false);
+        tmpt = mainTutorial.GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (on)
+        {
+            tmpt.color += new Color(1, 1, 1, 0.05f);
+
+            if (tmpt.color.a >= 1)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
         
     }
 
@@ -24,8 +36,8 @@ public class TutorialScript : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
+            on = true;
             mainTutorial.SetActive(true);
-            this.gameObject.SetActive(false);
         }
     }
 }
