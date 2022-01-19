1d0b93c34e22dc37333779b5cae3f6cf3b6badfb
EricRoman16 <1707234@mukilteo.wednet.edu>
2022-01-18 19:02:30 -0800

    small changes with the tutorials

--- Assets/Eric/Scripts/TutorialScript.cs
+++ Assets/Eric/Scripts/TutorialScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class TutorialScript : MonoBehaviour
 {
+    public GameObject mainTutorial;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainTutorial.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            mainTutorial.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
     }
 }
